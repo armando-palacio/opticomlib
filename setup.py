3a9a0548e29d419113ec690cd5ceb1e453b4dfa3
armando-palacio <armandopr3009@gmail.com>
2024-01-18 14:20:10 -0300

    :package: package creation and setup.py config

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opticomlib',
+    version='0.2',
+    description='Python package for optical communication systems.',
+    url='https://github.com/armando-palacio/opticomlib.git',
+    author='Armando Palacio',
+    author_email='armandopr3009@gmail.com',
+    license='MIT',
+    packages=['opticomlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    zip_safe=False,
+)
