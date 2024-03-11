7a3aa78ecc450f8f2ee7b5821d08aab24c5415ba
armando-palacio <armandopr3009@gmail.com>
2024-03-11 14:39:04 -0300

    update

--- opticomlib/__init__.py
+++ opticomlib/__init__.py
@@ -2,10 +2,9 @@ from .typing import *
 
 from .utils import *
 
-import os 
+import os
 
-if os.path.isfile('../VERSION.txt'):
-    __version__ = open('../VERSION.txt').read()
-else:
-    __version__ = None
+with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VERSION.txt')) as version_file:
+    __version__ = version_file.read().strip()
+
 del os
