372f810060aec9db0991f6f12192e3a5caf49aa8
armando-palacio <armando-palacio@users.noreply.github.com>
2024-03-18 02:27:17 +0000

    deploy: c8484a6d50b634cf7539bc2c7bda60155740ab58

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.4.10',
-    LANGUAGE: 'English',
+    VERSION: '0.4.11',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
