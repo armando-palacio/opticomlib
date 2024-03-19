aef2fa1b2986e4aa2da6754f1857fa42bd94ff4c
armando-palacio <armandopr3009@gmail.com>
2024-03-19 16:23:29 -0300

    test: electrical signal tests was added to `typing_test.py` file

--- tests/typing_test.py
+++ tests/typing_test.py
@@ -113,5 +113,123 @@ class TestBinarySequence(unittest.TestCase):
                 assert_(bits.ones() == 4)
                 assert_(bits.zeros() == 8)
 
+class TestElectricalSignal(unittest.TestCase):
+    inputs = [np.arange(100).tolist(), np.arange(100)] 
+
+    def test_init(self):
+        assert_raises(KeyError, lambda: electrical_signal())
+        assert_raises(ValueError, lambda: electrical_signal([0,1,2], [0,1,2,3]))
+        assert_raises(ValueError, lambda: electrical_signal([0,1,2,3], [0,1,2]))
+        assert_raises(TypeError, lambda: electrical_signal(signal='0,0.1,0.2,0.3'))
+        assert_raises(TypeError, lambda: electrical_signal(noise='0,0.1,0.2,0.3'))
+
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                
+                assert_equal(signal.signal, np.arange(100))
+                assert_equal(signal.noise, np.zeros(100))
+                assert_equal(signal.execution_time, None)
+                assert_equal(signal.len(), 100)
+
+    def test_print(self):
+        x = np.linspace(0, 1, 100)
+        y = np.sin(2*pi*5*x)
+        signal = electrical_signal(x, y)
+        try:
+            signal.print("something")
+        except Exception as e:
+            self.fail(f"signal.print() raised {type(e).__name__} unexpectedly!")
+
+    def test_getitem(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                assert_equal(signal[0][0], 0)
+                assert_equal(signal[:].signal, np.arange(100))
+                assert_equal(signal[4:-1].signal, np.arange(4,99))
+                assert_equal(signal.noise, np.zeros(100))
+                assert_raises(IndexError, lambda: signal[100])
+
+    def test_add_and_radd(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                
+                assert_equal((signal + signal).signal, np.arange(100)*2)
+                assert_equal((signal + 1).signal, np.arange(100)+1)
+                assert_equal((1 + signal).signal, np.arange(100)+1)
+                assert_raises(ValueError, lambda: signal + [0,3,0]) # Different length
+                
+    def test_sub_and_rsub(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                
+                assert_equal((signal - signal).signal, np.zeros(100))
+                assert_equal((signal - 1).signal, np.arange(100)-1)
+                assert_equal((1 - signal).signal, 1-np.arange(100))
+                assert_raises(ValueError, lambda: signal - [0,3,0]) # Different length
+
+    def test_mul_and_rmul(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                
+                assert_equal((signal * signal).signal, np.arange(100)**2)
+                assert_equal((signal * 2).signal, np.arange(100)*2)
+                assert_equal((2 * signal).signal, np.arange(100)*2)
+                assert_raises(ValueError, lambda: signal * [0,3,0]) # Different length
+    
+    def test_gt_and_lt(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                
+                assert_equal((signal + 1 > 0),  np.ones(100))
+                assert_equal((signal < 100), np.ones(100))
+
+    def test_call(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input)
+                assert_equal(signal('t').signal, np.fft.ifft(input))
+                assert_equal(signal('f').signal, np.fft.fft(input))
+                assert_equal(signal('w').signal, np.fft.fft(input))
+                assert_raises(ValueError, lambda: signal('z'))
+
+                assert_equal(signal('t', shift=True).signal, np.fft.ifftshift(np.fft.ifft(input)))
+    
+    def test_power(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input, noise=np.ones(100))
+                assert_equal(signal.power(), np.sum(np.abs(input + signal.noise)**2)/len(input))
+                assert_equal(signal.power('signal'), np.sum(np.abs(input)**2)/len(input))
+                assert_equal(signal.power('noise'), 1)
+                assert_raises(ValueError, lambda: signal.power('z'))
+
+    def test_abs(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input, noise=np.ones(100))
+                assert_equal(signal.abs(), np.abs(input + signal.noise))
+                assert_equal(signal.abs('signal'), np.abs(input))
+                assert_equal(signal.abs('noise'), np.ones(100))
+                assert_raises(ValueError, lambda: signal.abs('z'))
+
+    def test_phase(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input, noise=np.ones(100))
+                assert_equal(signal.phase(), np.unwrap(np.angle(input + signal.noise)))
+    
+    def test_apply(self):
+        for input in self.inputs:
+            with self.subTest(input_type=type(input)):
+                signal = electrical_signal(input, noise=np.ones(100))
+                assert_equal(signal.apply(np.abs).signal, np.abs(input))
+                assert_equal(signal.apply(np.abs).noise, np.abs(signal.noise))
+
 if __name__ == '__main__':
     unittest.main()
