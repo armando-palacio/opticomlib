5fd3dbce95a2a7e49b46beb2e242865e18aff209
armando-palacio <armando-palacio@users.noreply.github.com>
2024-04-04 23:03:12 +0000

    deploy: f65e5b49808c1d9c3ccf2c06cd96d5c0db58b082

--- _downloads/28621d512961f7bd67b1f606213b4bee/devices-1.py
+++ _downloads/28621d512961f7bd67b1f606213b4bee/devices-1.py
@@ -3,4 +3,4 @@ from opticomlib import gv
 
 gv(sps=32) # set samples per bit
 
-DAC('0 0 1 0 0', Vout=5, pulse_shape='gaussian', m=2).plot('r', lw=3).show()
+DAC('0 0 1 0 0', Vout=5, pulse_shape='gaussian', m=2).plot('r', lw=3, grid=True).show()
